b89a5d47db671ec592320b08bdbb31832f57df9d
anonymous191828281 <147885459+anonymous191828281@users.noreply.github.com>
2023-10-13 19:49:17 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -10,41 +10,12 @@ read -p "Tem certeza que deseja iniciar a experiência?(s/n): " yn
 case $yn in
         [sS] ) echo -e "\033[1;92m Iniciando....\033[0m";
 
-sleep 1
-
-clear
-
-sleep 2
-
-pkg update -y && pkg upgrade -y
-
-pkg install python -y
-
-pkg install git -y
-
-pip install tqdm
-
-pip install lolcat
-
-pkg install lynx -y
-
-pkg install toilet -y
-
-pkg install play-audio -y
-
-pkg install termimage -y
-
-pip install qrcode
-
-pip install pillow 
-
-mv /data/data/com.termux/files/home/downloads/chapter1 /data/data/com.termux/files/home
 
 # by al4n
 
 clear
 
-echo -e "\033[1;31m Depêndencias Instaladas!!\033[0m"
+echo -e "\033[1;31m YOU CANT SCAPE FROM THE TRUTH\033[0m"
 
 chmod +x *
 
