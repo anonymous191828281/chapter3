a7f923c3e8e0c9d10c588602fcb2b5ebe010e67e
anonymous191828281 <147885459+anonymous191828281@users.noreply.github.com>
2023-10-13 20:54:25 -0300

    Add files via upload

---
+++ start.py
@@ -0,0 +1,28 @@
+import os
+import time
+import qrcode
+while True:
+	def red(msg):
+		input(f"\033[5;49;91m{msg}\033[m")
+	def green(mkg):
+		input(f"\033[5;49;92m{mkg}\033[m")
+	def yellw(msgg):
+		input(f"\033[5;49;93m{msgg}\033[m")
+	def darkblue(msggu):
+		input(f"\033[5;49;94m{msggu}\033[m")
+	def no(msgu):
+		print(f"\033[7;49;34m{msgu}\033[m")
+	def clear():
+		os.system("clear")
+	clear()
+	os.system("dialog --msgbox nʇnoqǝɹɐɔʇuoʍʎǝɥʇ 60 60")
+	os.system("dialog --msgbox ssendeerg ruo edih tnac yeht 30 30")
+	os.system("dialog --msgbox US4igrQMtJaQo3Fszv0rKFGGbAGfX80lLCEj1erIuVOIOsVfUeu2aGYsimxP8uyBC2pyBpvmbSrasZET23Ep_C8iNx4KocBYgggKJ0RoM0g-key=blindness 40 40")
+	os.system("dialog --yesno you believe on GOD? 30 30")
+	aqr = open("/data/data/com.termux/files/home/chapterfinal/sacry.txt", "r")
+	clear()
+	print(aqr.read())
+	os.system("play-audio jumpscare.mp3")
+	os.system("dialog --msgbox they only seem like they care about YOUR causes 60 60")
+	os.system("dialog --msgbox thank you for playing 30 30")
+	break
